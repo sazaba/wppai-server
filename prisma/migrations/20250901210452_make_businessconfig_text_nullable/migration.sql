46bf5f8dd366bd98931aff020b48e9ba5a5b923a
Santiago Zappa <santiagozappab@gmail.com>
2025-09-01 16:05:53 -0500

    db update

---
+++ prisma/migrations/20250901210452_make_businessconfig_text_nullable/migration.sql
@@ -0,0 +1,13 @@
+-- AlterTable
+ALTER TABLE `businessconfig` MODIFY `descripcion` TEXT NULL,
+    MODIFY `servicios` TEXT NULL,
+    MODIFY `faq` TEXT NULL,
+    MODIFY `horarios` TEXT NULL,
+    MODIFY `disclaimers` TEXT NULL,
+    MODIFY `canalesAtencion` TEXT NULL,
+    MODIFY `enviosInfo` TEXT NULL,
+    MODIFY `extras` TEXT NULL,
+    MODIFY `metodosPago` TEXT NULL,
+    MODIFY `politicasDevolucion` TEXT NULL,
+    MODIFY `politicasGarantia` TEXT NULL,
+    MODIFY `promocionesInfo` TEXT NULL;
