dc40e7dd28ba6e26e506870dfbe85163546df9fb
Santiago Zappa <santiagozappab@gmail.com>
2025-12-05 19:26:34 -0500

    fix: codigo limpio y gitignore actualizado

--- src/routes/test.routes.ts
+++ src/routes/test.routes.ts
@@ -6,11 +6,16 @@ import {
     eliminarTest
 } from "../controllers/testModel.controller";
 
+// 👇 1. IMPORTA TU MIDDLEWARE DE AUTH
+// (Revisa si tu archivo se llama auth.middleware.ts o similar)
+import { verificarJWT } from '../middleware/auth.middleware'
 const router = Router();
 
-router.post("/", crearTest);
-router.get("/", listarTests);
-router.get("/:id", obtenerTest);
-router.delete("/:id", eliminarTest);
+// 👇 2. AGREGA EL MIDDLEWARE ANTES DE LOS CONTROLADORES
+// Esto asegura que req.user exista cuando llegue al controlador
+router.post("/", verificarJWT , crearTest);
+router.get("/", verificarJWT , listarTests);
+router.get("/:id", verificarJWT , obtenerTest);
+router.delete("/:id", verificarJWT , eliminarTest);
 
 export default router;
